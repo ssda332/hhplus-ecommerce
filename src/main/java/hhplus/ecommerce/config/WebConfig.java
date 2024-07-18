f8bef76b3e2e1672a544bdc2eaac3517f9751a05
ssda332 <tititiqkfk3@gmail.com>
2024-07-19 04:54:15 +0900

    feat: request/response 필터 로깅 추가

---
+++ src/main/java/hhplus/ecommerce/config/WebConfig.java
@@ -0,0 +1,21 @@
+package hhplus.ecommerce.config;
+
+import hhplus.ecommerce.member.interceptor.MemberInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final MemberInterceptor memberInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(memberInterceptor)
+                .addPathPatterns("/balance/**");
+    }
+}
